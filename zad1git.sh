dd16df10976b255f25d80d67c33c971fae6b51a4
Dominika <129169321+dominiquelt@users.noreply.github.com>
2023-04-21 21:29:51 +0200

    Add files via upload
    bez zmian

---
+++ zad1git.sh
@@ -0,0 +1,114 @@
+
+#MAIN
+#level1
+git commit
+git commit 
+
+#level2
+git branch bugFix
+git checkout bugFix
+
+#level3
+git branch bugFix; git checkout bugFix
+git commit
+git checkout main; git commit
+git merge bugFix
+
+#level4
+git branch bugFix
+git checkout bugFix
+git commit
+git chekout main
+git commit
+git checkout bugFix
+git rebase main
+
+#level5
+git checkout C4
+
+#level6
+git branch -f main C6
+git branch -f bugFix C0
+git checkout C1
+
+#level7
+git reset HEAD^
+git checkout pushed
+git revert pushe 
+
+#level8
+git cherry-pick C3 C4 C7
+
+#level9
+git rebase -i HEAD~4
+
+#level10
+git checkout main
+git cherry-pick C4
+
+#level11
+git rebase -i HEAD~2
+git commit --amend
+git rebase -i HEAD~2
+git branch -f main C3''
+
+#level12
+git checkout main
+git cherry-pick C2
+git branch -f main C1
+git cherry-pick C2
+git chery-pick C3
+
+#level13
+git tag v0 C1
+git tag v1 C2
+git checkou C2
+
+#level14
+git commit
+
+
+#REMOTE
+
+#level1
+git clone
+
+#level2
+git commit
+git checkout o/main
+git commit
+
+#level3
+git fetch
+
+#level4
+git pull
+
+#level5
+git clone
+git checkout main
+git fakeTeamwork
+git fakeTeamwork
+git fetch
+git commit
+git megre o/main
+
+#level6
+git commit
+git commit
+git push
+
+#level7
+git clone 
+git fakeTeamwork
+git commit
+git pull --rebase
+git push
+
+#level8
+git branch feature
+git checkout -f main C1
+git checkout feature
+git push
+
+
